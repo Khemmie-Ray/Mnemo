ed4b886ed2bec21831b55e99d9feeb83d248f963
Khemmie-Ray <atokemmy@gmail.com>
2026-05-01 23:13:03 +0100

    mockusdc contract

--- contract/script/MnemoRegistry.s.sol
+++ contract/script/MnemoRegistry.s.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.26;
 
 import {Script} from "forge-std/Script.sol";
 import {MnemoRegistry} from "../src/MnemoRegistry.sol";
+import {MockUSDC} from "../src/MockUSDC.sol";
 
 contract MnemoRegistryScript is Script {
     MnemoRegistry public mnemoregistry;
+    MockUSDC public usdc;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
+
+        usdc = new MockUSDC();
 
         mnemoregistry = new MnemoRegistry();
 
